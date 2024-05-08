3df68e93b2f015d96c2dbf09c0c9738bef05ff49
unknown <legonzalez@fi.uba.ar>
2024-05-07 22:43:18 -0300

    ej 4 finalizado

--- ej4.m
+++ ej4.m
@@ -3,6 +3,11 @@ rs = 0.001;
 M = 10000;
 fs = 200;
 
+x1 = csvread("Archivos/eeg_ojos_abiertos_t7.csv");
+x2 = csvread("Archivos/eeg_ojos_cerrados_t7.csv");
+N_x1 = length(x1);
+N_x2 = length(x2);
+
 N = 280; % 255
 F = [0 3*2/fs 5*2/fs 1];
 A = [1 1 0 0];
@@ -44,6 +49,8 @@ h4 = firpm(N4, F4, A4, V4);
 H4 = fft(h4, M);
 
 w = linspace(0, fs, M);
+
+% Graficos de filtros
 
 figure
 hold on
@@ -113,12 +120,117 @@ grid on
 title('Filtro de ondas Gamma')
 xlim([0, 45])
 plot(w, abs(H4), color = 'blue')
-
 line([31 45], [1.01 1.01], color = 'red', linestyle = '--')
 line([31 45], [0.99 0.99], color = 'red', linestyle = '--')
 line([0 29], [0.001 0.001], color = 'red', linestyle = '--')
 line([0 29], [-0.001 -0.001], color = 'red', linestyle = '--')
 xlabel('f[Hz]')
 ylabel('|H_{G}(f)|')
-
 hold off
+
+%------------------------------------------***************---------------------------------------------
+
+%sintesis de señales
+
+ruido = normrnd(0,1,1,N); 
+
+[a_13_1, G_13_1] = ar_model(x1, 13); %ojos abiertos
+
+u_13_1 = ruido;
+x_13_1 = filter(G_13_1, [1 -a_13_1'], u_13_1);
+
+[a_13_2, G_13_2] = ar_model(x2, 13);   %ojos cerrados
+
+u_13_2 =  ruido;
+x_13_2 = filter(G_13_2, [1 -a_13_2'], u_13_2);
+
+%filtrado de señales reales
+
+y_1 = conv(h,x1);
+y1_1 = conv(h1, x1);
+y2_1 = conv(h2, x1);
+y3_1 = conv(h3, x1);
+y4_1 = conv(h4, x1);
+
+y_2 = conv(h,x2);
+y1_2 = conv(h1, x2);
+y2_2 = conv(h2, x2);
+y3_2 = conv(h3, x2);
+y4_2 = conv(h4, x2);
+
+%filtrado de las señales sintetizadas
+
+y_13_1 = conv(h,x_13_1);
+y1_13_1 = conv(h1, x_13_1);
+y2_13_1 = conv(h2, x_13_1);
+y3_13_1 = conv(h3, x_13_1);
+y4_13_1 = conv(h4, x_13_1);
+
+y_13_2 = conv(h,x_13_2);
+y1_13_2 = conv(h1, x_13_2);
+y2_13_2 = conv(h2, x_13_2);
+y3_13_2 = conv(h3, x_13_2);
+y4_13_2 = conv(h4, x_13_2);
+
+%calculo de varianza delas señales reales
+
+var_y_1 = [var(y_1) var(y1_1) var(y2_1) var(y3_1) var(y4_1)];
+var_y_2 = [var(y_2) var(y1_2) var(y2_2) var(y3_2) var(y4_2)];
+
+%calculo de varianza de señales sintetizadas
+
+var_y_13_1 = [var(y_13_1) var(y1_13_1) var(y2_13_1) var(y3_13_1) var(y4_13_1)];
+var_y_13_2 = [var(y_13_2) var(y1_13_2) var(y2_13_2) var(y3_13_2) var(y4_13_2)];
+
+% grafico de potencias
+
+figure
+hold on
+grid on
+xticks([1 2 3 4 5])
+plot(var_y_1, 'DisplayName', 'Potencia de la señal real');
+plot(var_y_13_1, 'DisplayName', 'potencia de la señal sintetizada');
+xticklabels({'D', 'T', 'A', 'B', 'G'});
+title('Potencia de las señales de ojos abiertos')
+hold off
+legend
+
+figure
+hold on
+grid on 
+xticks([1 2 3 4 5])
+plot(var_y_2, "DisplayName", "Potencia de la señal real", color = 'blue');
+plot(var_y_13_2, "DisplayName", "potencia de la señal sintetizada", color = 'red');
+xticklabels({'D', 'T', 'A', 'B', 'G'});
+legend
+title('Potencia de las señales de ojos cerrados')
+hold off
+
+function [a, G] = ar_model(x, P)
+
+    N = length(x); %largo del vector x
+
+    %calculamos la funcion de autocorrelacion de x
+    R_x = xcorr(x, 'biased');
+
+    %calculamos la matriz R
+
+    R = zeros(P, P);
+
+    for i = 0:P-1
+
+        R(i+1, :) = R_x(N-i:N+P-1-i);
+
+    end
+
+    %defino el vector r
+    r = R_x(N+1:N+P);
+
+    %estimo los coeficientes de a
+    a = R^(-1) * r;
+
+    %estimamos la ganancia
+
+    G = (R_x(N) - sum( a'*R_x(N+1:N+P) ))^(1/2);
+
+end
